f1651f5725221b6938a39d5512550a4bc175e0ee
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-04-19 19:41:28 +0300

    add toggle method to javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,28 @@
-$(document).ready(function() {
-    $(".clickable").click(function(){
-        $(".design-hidden").slideToggle();
-        $(".design-showing").slideToggle();
+$(document).ready(function () {
+  $(".para1").click(function () {
+    $(".para1").toggle(function () {
+      $(".img1").show(function () {
+        $("#h4a").css('font-weight', 'normal');
+      });
     });
+  });
+
+  $(".para2").click(function () {
+    $(".para2").toggle(function () {
+      $(".img2").show(function () {
+        $("#h4b").css('font-weight', 'normal');
+      });
+    });
+  });
+
+  $(".para3").click(function () {
+    $(".para3").toggle(function () {
+      $(".img3").show(function () {
+        $("#h4c").css('font-weight', 'normal');
+      });
+    });
+  });
+});
 
 
 
@@ -12,7 +32,6 @@ $(document).ready(function() {
 
 
 
-}
     
         
 
