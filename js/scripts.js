bb36d81b3e4496e540585f496b474bb93d4d4c28
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-04-19 20:25:56 +0300

    add images to images file and new java script file

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,28 @@
 $(document).ready(function () {
+    $(".img1").click(function () {
+        $(".img1").toggle(function () {
+          $(".para1", ).show(function () {
+            $("#h3a").css('font-weight', 'bold');
+          });
+        });
+      });
+    
+      $(".img2").click(function () {
+        $(".img2").toggle(function () {
+          $(".para2").show(function () {
+            $("#h3b").css('font-weight', 'bold');
+          });
+        });
+      });
+    
+      $(".img3").click(function () {
+        $(".img3").toggle(function () {
+          $(".para3").show(function () {
+            $("#h3c").css('font-weight', 'bold');
+          });
+        });
+      });
+
   $(".para1").click(function () {
     $(".para1").toggle(function () {
       $(".img1").show(function () {
@@ -28,7 +52,15 @@ $(document).ready(function () {
     alert(user + " your message has been recieved successfully and thanks for keeping in touch");
 
   });
+  $(".prjct").hide();
+  $("pjct").animate({opacity:0.2}, 1000);
+  $(".pic1").hover(function () {
+    $(this).find(".prjct").toggle(400);
+  });
+
 });
+
+
 
 
 
